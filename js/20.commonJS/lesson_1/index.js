d2bfc2ea4a25437902f9ebf2bec7c3b477ec30f8
Vitaly Zubov <Zubov.VP@yandex.ru>
2022-08-09 23:32:55 +0300

    Add task for practice (21.Tests_with_choose_in_array)

---
+++ js/20.commonJS/lesson_1/index.js
@@ -0,0 +1,4 @@
+let math = require('./math');
+
+let res = math.square(2) + math.cube(3);
+console.log(res);
---
+++ js/20.commonJS/lesson_1/math.js
@@ -0,0 +1,6 @@
+module.exports.square = function(num) {
+    return num * num;
+}
+module.exports.cube   = function (num) {
+    return num * num * num;
+}
